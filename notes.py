0ce2ff36d24abf7aee7289c2e188aaaf245625e6
eric-d-culver <eric.d.culver@gmail.com>
2015-06-17 18:43:25 -0600

    adding comments for future features

--- notes.py
+++ notes.py
@@ -4,9 +4,9 @@ class App:
 	def __init__(self, master):
 		self.outerFrame = Frame(master)
 		self.outerFrame.pack(side = TOP)
-		self.flows = []
-		self.userHeight = 10
-		self.userWidth = 30
+		self.flows = [] # A list of all the flows
+		self.userHeight = 10 # called userHeight to remind me to make it user configureable
+		self.userWidth = 30 # called userWidth to remind me to make it user configureable
 		flow = self.addFlow()
 		return
 
@@ -16,7 +16,7 @@ class App:
 		newFlow['frame'].pack(side = LEFT)
 		newFlow['title'] = Label(newFlow['frame'], text = "Flow")
 		newFlow['title'].pack(side = TOP)
-		newFlow['cards'] = []
+		newFlow['cards'] = [] # A list of all the cards in the flow
 		self.addCard(newFlow).focus()
 		self.flows.append(newFlow)
 		return newFlow
@@ -34,7 +34,7 @@ class App:
 		flow['cards'].append(newCard)
 		return newCard
 
-	def moveFlow(self, flow, event):
+	def moveFlow(self, flow, event): # only moves right for now, add left
 		for index, f in enumerate(self.flows):
 			if f == flow:
 				if index+1 < len(self.flows):
@@ -43,7 +43,7 @@ class App:
 					self.addFlow()
 		return "break"
 
-	def moveCard(self, flow, event):
+	def moveCard(self, flow, event): # only moves down when Enter (Return) is pressed, perhaps add Up and Down arrows to move?
 		for index, card in enumerate(flow['cards']):
 			if card == event.widget:
 				if index+1 < len(flow['cards']):
@@ -54,8 +54,8 @@ class App:
 
 	def update_card_size(self, event):
 		curCard = event.widget
-		#lines = int(float(curCard.index(END)))
-		#print lines
+		#lines = int(float(curCard.index(END))) # Returns the number of actual lines, as in, you have to press Enter to change it.
+		# Need a function that returns the number of displayed lines.
 		text_height = 1 + len(curCard.get("1.0", END))/curCard.cget("width")
 		if curCard.cget("height") < text_height:
 			curCard.config(height = text_height)
